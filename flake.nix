67d19e0e7aadec4e2ddb6e4f0c9db22d5c5ba45d
aktaboot <akhtaboot@protonmail.com>
2024-10-17 20:46:11 +0200

    flake.nix: use oxalica's rust-overlay

--- flake.nix
+++ flake.nix
@@ -2,56 +2,51 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nmattia/naersk";
-    mozillapkgs = {
-      url = "github:mozilla/nixpkgs-mozilla";
-      flake = false;
-    };
+      rust-overlay = { 
+        url = "github:oxalica/rust-overlay";
+      };
   };
 
-  outputs = { self, nixpkgs, utils, naersk, mozillapkgs }:
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay, ...}:
   utils.lib.eachDefaultSystem (system: let
-    pkgs = nixpkgs.legacyPackages."${system}";
+    overlays = [(import rust-overlay)];
 
-      # Get a specific rust version
-      mozilla = pkgs.callPackage (mozillapkgs + "/package-set.nix") {};
-      rust = (mozilla.rustChannelOf {
-        date = "2020-12-23";
-        channel = "nightly";
-        sha256 = "LbKHsCOFXWpg/SEyACfzZuWjKbkXdH6EJKOPSGoO01E="; # set zeros after modifying channel or date
-      }).rust;
-      rust-src = (mozilla.rustChannelOf {
-        date = "2020-12-23";
-        channel = "nightly";
-        sha256 = "LbKHsCOFXWpg/SEyACfzZuWjKbkXdH6EJKOPSGoO01E="; # set zeros after modifying channel or date
-      }).rust-src;
+    pkgs = import nixpkgs { inherit system overlays;};
 
-      naersk-lib = naersk.lib."${system}".override {
-        cargo = rust;
-        rustc = rust;
-      };
+      # # Get a specific rust version
+      # mozilla = pkgs.callPackage (mozillapkgs + "/package-set.nix") {};
+      # rust = (mozilla.rustChannelOf {
+      #   date = "2020-12-23";
+      #   channel = "nightly";
+      #   sha256 = "LbKHsCOFXWpg/SEyACfzZuWjKbkXdH6EJKOPSGoO01E="; # set zeros after modifying channel or date
+      # }).rust;
+      # rust-src = (mozilla.rustChannelOf {
+      #   date = "2020-12-23";
+      #   channel = "nightly";
+      #   sha256 = "LbKHsCOFXWpg/SEyACfzZuWjKbkXdH6EJKOPSGoO01E="; # set zeros after modifying channel or date
+      # }).rust-src;
+
     in rec {
-      packages.my-project = naersk-lib.buildPackage {
-        pname = "dijo";
-        version = "0.2.7";
-        root = ./.;
-      };
-      defaultPackage = packages.my-project;
-      apps.my-project = utils.lib.mkApp {
-        drv = packages.my-project;
-      };
-      defaultApp = apps.my-project;
+      # packages.my-project = naersk-lib.buildPackage {
+      #   pname = "dijo";
+      #   version = "0.2.7";
+      #   root = ./.;
+      # };
+      # defaultPackage = packages.my-project;
+      # apps.my-project = utils.lib.mkApp {
+      #   drv = packages.my-project;
+      # };
+      # defaultApp = apps.my-project;
       devShell = pkgs.mkShell {
         nativeBuildInputs = [
-          rust
-          rust-src
           pkgs.rust-analyzer
           pkgs.cargo
           pkgs.openssl
           pkgs.ncurses
         ];
-        shellHook = ''
-          export RUST_SRC_PATH="${rust-src}/lib/rustlib/src/rust/library"
-        '';
+        # shellHook = ''
+        #   export RUST_SRC_PATH="${rust-src}/lib/rustlib/src/rust/library"
+        # '';
       };
     });
   }
